7a84073c31c088d207d7f302b58dcca2646ee529
Duncan Mak <duncan.mak@xamarin.com>
2015-07-09 21:44:09 -0400

    Fix up wording a little more

--- bad.ts
+++ bad.ts
@@ -3,4 +3,4 @@
 import { pad } from 'lodash'
 
 console.log(pad('abc', 8));
-console.log(_.pad('abc', 8)); // ERROR: This line shouldn't compile
+console.log(_.pad('abc', 8)); // BAD: This line shouldn't compile, but tsc doesn't catch it
--- good.ts
+++ good.ts
@@ -3,4 +3,4 @@
 import _ = require('lodash');
 
 console.log(_.pad('abc', 8));
-
+console.log(pad('abc', 8)); // GOOD: This line doesn't compile, and that's correct
